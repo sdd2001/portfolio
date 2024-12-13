30f619fa3b71beb51c2f4893295e672951c9ad18
Sean De Decker <sean.dedecker@outlook.com>
2024-12-13 11:57:10 +0100

    Fix failing tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,29 +1,20 @@
-import { TestBed } from '@angular/core/testing'
+import { ComponentFixture, TestBed } from '@angular/core/testing'
 import { AppComponent } from './app.component'
 
-describe('AppComponent', () => {
-    beforeEach(async () => {
-        await TestBed.configureTestingModule({
+describe('AppComponent', (): void => {
+    let fixture: ComponentFixture<AppComponent>
+    let app: AppComponent
+
+    beforeEach((): void => {
+        TestBed.configureTestingModule({
             imports: [AppComponent]
-        }).compileComponents()
+        })
+
+        fixture = TestBed.createComponent(AppComponent)
+        app = fixture.componentInstance
     })
 
-    it('should create the app', () => {
-        const fixture = TestBed.createComponent(AppComponent)
-        const app = fixture.componentInstance
+    it('should create the app', (): void => {
         expect(app).toBeTruthy()
-    })
-
-    it(`should have the 'portfolio' title`, () => {
-        const fixture = TestBed.createComponent(AppComponent)
-        const app = fixture.componentInstance
-        expect(app.title).toEqual('portfolio')
-    })
-
-    it('should render title', () => {
-        const fixture = TestBed.createComponent(AppComponent)
-        fixture.detectChanges()
-        const compiled = fixture.nativeElement as HTMLElement
-        expect(compiled.querySelector('h1')?.textContent).toContain('Hello, portfolio')
     })
 })
